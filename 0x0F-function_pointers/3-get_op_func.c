1d9c7f1330b7d674fd0551e6a5b0b6450be50120
weingeorge <osiakhile@gmail.com>
2023-04-21 21:47:47 -0700

    get func

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,8 +1,8 @@
 #include "3-calc.h"
 
 /**
- * get_op_func - a function pointer that selects the correct function to perform
- * operation asked by the user
+ * get_op_func - a function pointer that selects a correct function to perform
+ * operation asked by user
  * @s: the operator given by the user
  *
  * Return: pointer to a function that corresponds to the
